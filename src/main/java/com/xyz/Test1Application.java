615b9e579c51d41e91d0f73c93deda3ffffd52fa
biswajit <paleibiswajit30@gmail.com>
2025-10-15 15:28:18 +0530

    ok

--- src/main/java/com/xyz/Test1Application.java
+++ src/main/java/com/xyz/Test1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test1Application {
 
 	public static void main(String[] args) {
+                 String s="java";
 		SpringApplication.run(Test1Application.class, args);
 	}
 
